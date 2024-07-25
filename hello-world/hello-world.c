b4a1f7a899ede3d1518f39eb07d84ffc60afad79
Michael Caballero <mcaballero.dev@gmail.com>
2024-07-25 02:16:02 -0700

    Create 'Hello, world' program

---
+++ hello-world/hello-world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Define a function main that retuns an integer
+int 
+main()
+{
+	// printf: prints a sequence of characters given as an argument
+	printf("Hello, world!\n"); // '\n' represents a newline character
+	return 0;
+}
