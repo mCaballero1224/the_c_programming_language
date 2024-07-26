56974d5e69c1eb15dc65df725a50a567e1f6f008
Michael Caballero <mcaballero.dev@gmail.com>
2024-07-26 05:43:47 -0700

    Write a program that keeps track of characters, including blanks, newlines, and tabs.

---
+++ chapter1/specialcount/specialcount.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define TAB 9
+#define NEWLINE 10
+#define BLANK 32
+
+int
+main()
+{
+	int c, nc, nl, nt, nb; /* init variables */
+
+	nc = nl = nt = nb = 0; /* set counters to 0 */
+
+	/* read user input till EOF received 
+	 	store input in c*/
+	while ((c = getchar()) != EOF) 
+	{
+		nc++; /* increment character count */
+		if (c == TAB)
+			nt++; /* increment tab count */
+		if (c == NEWLINE)
+			nl++; /* increment newline count */
+		if (c == BLANK)
+			nb++; /* increment newline count */
+	}
+
+	printf("Tab Count: %d\n", nt);
+	printf("Newline Count: %d\n", nl);
+	printf("Blank Count: %d\n", nb);
+	printf("Total Character Count: %d\n", nc);
+}
