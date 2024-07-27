eb8addab7deb8c14fba1250777ebdab5f2e9f2b3
Michael Caballero <mcaballero.dev@gmail.com>
2024-07-27 04:00:33 -0700

    Start writing a program that prints each word line by line

---
+++ chapter1/wordline/wordline.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define IN 1	/* inside a word */
+#define OUT 0	/* outside a word */
+
+int
+main()
+{
+	char c;
+	int len, state;
+
+	state = OUT;
+
+	while ((c = getchar()) != EOF)
+	{
+
+	}
+
+	return 0;
+}
